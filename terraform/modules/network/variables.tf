e93862adf56aa153c75870e997a4d69223a27c77
とんび（Tombee） <admin@tombee-studio.com>
2026-06-14 14:00:24 +0900

    Ftr: Add AWS network Terraform module
    VPC, subnets, NAT GW, security groups
    
    Task: 101

---
+++ terraform/modules/network/variables.tf
@@ -0,0 +1,33 @@
+variable "project" {
+  description = "Project name used for resource naming"
+  type        = string
+}
+
+variable "env" {
+  description = "Environment name (dev or prd)"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.10.0/24", "10.0.11.0/24"]
+}
+
+variable "availability_zones" {
+  description = "Availability zones to deploy into"
+  type        = list(string)
+  default     = ["ap-northeast-1a", "ap-northeast-1c"]
+}
